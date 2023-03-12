44f2026278f9653ed900e08f4b0de16c1c2289ad
石渡拓海 <takumi-ishiwata@c-fo.com>
2023-03-12 13:37:26 +0900

    Add Card Section

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,11 @@ function App() {
         Framer Motion Samples
       </h1>
       <div className="h-[90%]">
-        <Card />
+        <div>
+          <h2 className="text-left text-2xl font-bold text-[#212121]">Card</h2>
+          <hr className="mb-4 border-[1.5px] text-[#000] opacity-[0.87]" />
+          <Card />
+        </div>
       </div>
     </div>
   );
